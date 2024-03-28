efa89a8a4f3ea0b5e04fb6e16de9223c652172f2
Uday <udaypratap1721@g,ail.com>
2024-03-28 21:22:42 +0530

    tes_frontend

--- src/App.js
+++ src/App.js
@@ -1,56 +1,55 @@
-import React from 'react';
-import Login from './Login';
-import Sidebar from './layout/ExploreMarkets';
-import table from './layout/screen/Profile'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Kyc from './layout/screen/Kyc';
-import Profile_Second from './layout/screen/Profile_Second';
-import AuthService from './services/Auth';
-import ExploreMarkets from './layout/ExploreMarkets';
-import Dashboard from '../src/layout/Dashboard';
-import Portfolio from '../src/layout/Portfolio';
-import WatchList from './layout/WatchList';
-import AllOrders from './layout/AllOrders';
+import React from "react";
+import Login from "./Login";
+import Sidebar from "./layout/ExploreMarkets";
+import table from "./layout/screen/Profile";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Kyc from "./layout/screen/Kyc";
+import Profile_Second from "./layout/screen/Profile_Second";
+import AuthService from "./services/Auth";
+import ExploreMarkets from "./layout/ExploreMarkets";
+import Dashboard from "../src/layout/Dashboard";
+import Portfolio from "../src/layout/Portfolio";
+import WatchList from "./layout/WatchList";
+import AllOrders from "./layout/AllOrders";
+import Register from "./layout/screen/Register";
+// import R from "./layout/screen/Register.css";
 
 function App() {
-
   const userData = AuthService.getCurrentUser();
-  if(!userData){
+  if (!userData) {
     return (
       <>
-          <BrowserRouter>
+        <BrowserRouter>
           <Routes>
-            <Route path='*' Component={Login}/>
+            <Route path="register" Component={Register} />
+            <Route path="*" Component={Login} />
           </Routes>
-          </BrowserRouter>
+        </BrowserRouter>
       </>
-    )
+    );
   }
   return (
     <>
-        <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          {/* <Route path='/' Component={Login}/> */}
-          <Route path='/' Component={Sidebar}/>
+          <Route path="/" Component={Login} />
+          <Route path="/" Component={Sidebar} />
 
-          <Route path='profile' Component={ExploreMarkets}/>
-          <Route path='dashboard' Component={Dashboard}/>
-          <Route path='all-orders' Component={AllOrders}/>
-          <Route path='portfolio' Component={Portfolio}/>
-          <Route path='watchlist' Component={WatchList}/>
+          <Route path="profile" Component={ExploreMarkets} />
+          <Route path="dashboard" Component={Dashboard} />
+          <Route path="all-orders" Component={AllOrders} />
+          <Route path="portfolio" Component={Portfolio} />
+          <Route path="watchlist" Component={WatchList} />
 
-          <Route path='explore-market' Component={ExploreMarkets}/>
+          <Route path="explore-market" Component={ExploreMarkets} />
 
-
-
-          
-          <Route path='table' Component={table}/>
-          <Route path='kyc' Component={Kyc}/>
-          <Route path='profilesecond' Component={Profile_Second}/>
+          <Route path="table" Component={table} />
+          <Route path="kyc" Component={Kyc} />
+          <Route path="profilesecond" Component={Profile_Second} />
         </Routes>
-        </BrowserRouter>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
